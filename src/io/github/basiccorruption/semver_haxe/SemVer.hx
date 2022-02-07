e66f7fbe77956e5ef0626b6bb1f10cde05a20c80
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2022-02-07 16:30:58 -0500

    Create SemVer.hx

---
+++ src/io/github/basiccorruption/semver_haxe/SemVer.hx
@@ -0,0 +1,24 @@
+package io.github.basiccorruption.semver_haxe;
+
+/**
+ * ...
+ * @author SidGames5
+ */
+class SemVer 
+{
+
+	public function new() 
+	{
+		
+	}
+	
+	public static function string(?major:Int = 1, ?minor:Int = 0, ?patch:Int = 0, ?build:Int = 1, ?stage:String = "release"):String {
+		var ma = Std.string(major);
+		var mi = Std.string(minor);
+		var pa = Std.string(patch);
+		var bu = Std.string(build);
+		var x = ma + "." + mi + "." + pa + "-" = stage + "." + build
+		return x;
+	}
+	
+}
